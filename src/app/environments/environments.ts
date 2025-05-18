13c0584b636f22259caf75c0c69d0002e7b36973
CristianoGheu <120095921+CristianoGheu@users.noreply.github.com>
2025-05-18 23:31:55 +0200

    Delete src directory

--- src/app/environments/environments.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  token:"",
-  temperatureApi: "https://global-warming.org/api/temperature-api",
-  co2Api: "https://global-warming.org/api/co2-api",
-  methaneApi: "https://global-warming.org/api/methane-api",
-  no2Api: "https://global-warming.org/api/nitrous-oxide-api",
-  polarIceApi: "https://global-warming.org/api/arctic-api"
-};
