3ae7da8f2d3e1f44bfade07816ce2b29d1b59dba
CristianoGheu <120095921+CristianoGheu@users.noreply.github.com>
2025-05-18 23:35:12 +0200

    Update environments.prod.ts

--- src/app/environments/environments.prod.ts
+++ src/app/environments/environments.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-    production: true,
-    firebaseConfig :{
-      apiKey: "AIzaSyA_JzoYy0mxlHSby8wyHJGRQbLGKUP0904",
-      authDomain: "progetto-finale-front-end-dev.firebaseapp.com",
-      projectId: "progetto-finale-front-end-dev",
-      storageBucket: "progetto-finale-front-end-dev.firebasestorage.app",
-      messagingSenderId: "893342518002",
-      appId: "1:893342518002:web:162b3fb87166d7278f5e6b",
-      measurementId: "G-LM4NWE2P7G"
-    }
+    production: true
   };
